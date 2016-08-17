9537a5e1abb02b5f0bc3ec3ccec97dcfe1c49c9f
leovimartinez <leovioz@hotmail.com>
2016-08-16 21:04:17 -0500

    Se agrega el modelo User y la migracion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817013530) do
+ActiveRecord::Schema.define(version: 20160817020109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 20160817013530) do
   end
 
   add_index "tasks", ["list_id"], name: "index_tasks_on_list_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "lastname"
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "usuarios", force: :cascade do |t|
     t.string   "nombre"
